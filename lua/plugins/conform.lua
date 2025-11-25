83eef0eac7b49bc990ca8b29ff8aca98bb40bcd7
Mattia Marini <marmattia26@gmail.com>
2025-11-25 12:58:17 +0100

    Added ruff to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -31,6 +31,24 @@ vim.api.nvim_create_user_command("Format", function(args)
   require("conform").format({ async = true, lsp_format = "fallback", range = range })
 end, { range = true })
 --]]
+local opts = {
+  formatters_by_ft = {
+    python = { "ruff_format", "ruff_organize_imports" },
+  },
+  notify_no_formatters = true,
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+  -- Set up format-on-save
+  -- format_on_save = { timeout_ms = 500 },
+  -- Customize formatters
+  -- formatters = {
+  --   shfmt = {
+  --     prepend_args = { "-i", "2" },
+  --   },
+  -- },
+}
+
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
@@ -45,28 +63,7 @@ return {
       desc = "Format buffer",
     },
   },
-  ---@module "conform"
-  ---@type conform.setupOpts
-  opts = {
-    -- Define your formatters
-    -- formatters_by_ft = {
-    --   lua = { "stylua" },
-    --   python = { "isort", "black" },
-    --   javascript = { "prettierd", "prettier", stop_after_first = true },
-    -- },
-    -- Set default options
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-    -- Set up format-on-save
-    -- format_on_save = { timeout_ms = 500 },
-    -- Customize formatters
-    -- formatters = {
-    --   shfmt = {
-    --     prepend_args = { "-i", "2" },
-    --   },
-    -- },
-  },
+  opts = opts,
   -- init = function()
   --   -- If you want the formatexpr, here is the place to set it
   --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
