e09bf360f2dc0fce0383aa7bef84bacb6bfecb4c
Mattia Marini <marmattia26@gmail.com>
2026-04-08 15:23:41 +0200

    Fixed ctrl bug

--- lua/utils/quickfix.lua
+++ lua/utils/quickfix.lua
@@ -1,13 +1,13 @@
 local function remove_qf_item()
-  print("REMOVING")
+  -- print("REMOVING")
+  -- print("lines")
+  -- print(vim.inspect(qf_lines))
 
   local qf_lines = vim.fn.getqflist()
 
   local cursor_pos_row = vim.api.nvim_win_get_cursor(0)[1];
 
   table.remove(qf_lines, cursor_pos_row)
-  print("lines")
-  print(vim.inspect(qf_lines))
 
   vim.fn.setqflist(qf_lines, 'r')
   vim.cmd("copen")
